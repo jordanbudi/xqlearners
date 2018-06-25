8b7d98aeb18f8f47bc90e9aed2713f707045cdaf
Jordan Budisantoso <jbudisantoso@wlapcs.org>
2018-06-25 02:19:28 -0400

    fixed text message, separated message-->blank-->message

--- models/text.rb
+++ models/text.rb
@@ -5,7 +5,19 @@ def sendMessage(phoneNumber, path, name)
     @client.messages.create(
       from: '+19093452633',
       to: phoneNumber,
-      body: "Thanks for playing, " + name +"! \nHere is your XQ Learner Goal!\nLearn More about XQ Learner Goals at:\nhttps://xqsuperschool.org/xq-schools/xq-learner-goals\n ",
+      body: "Thanks for playing, " + name +"! \nHere is your XQ Learner Goal:",
+      #media_url: path,
+      )
+    sendMessage2(phoneNumber, name)
+end
+
+
+def sendMessage2(phoneNumber, name)
+@client = Twilio::REST::Client.new('ACe26df60544f6832b428cd1bc02bcd32e','51869ceac9a4ab715494ff83db72dcc3')
+    @client.messages.create(
+      from: '+19093452633',
+      to: phoneNumber,
+      body: "Learn More about XQ Learner Goals at:\nhttps://xqsuperschool.org/xq-schools/xq-learner-goals",
       #media_url: path,
       )
 end
