f39da2ae044fd6f0fb1d02d7b4fd49555f04ef04
Jordan Budisantoso <jbudisantoso@wlapcs.org>
2018-06-25 03:41:42 -0400

    changed to adapt to no name

--- models/text.rb
+++ models/text.rb
@@ -1,11 +1,18 @@
 require 'twilio-ruby'
 
 def sendMessage(phoneNumber, path, name)
+  if (name == "First Name")
+    name = ""
+  else
+    name = ", " + name
+  end
+
+
   @client = Twilio::REST::Client.new('ACe26df60544f6832b428cd1bc02bcd32e','51869ceac9a4ab715494ff83db72dcc3')
     @client.messages.create(
       from: '+19093452633',
       to: phoneNumber,
-      body: "Thanks for playing, " + name +"! \nHere is your XQ Learner Goal:",
+      body: "Thanks for playing" + name + "! \nHere is your XQ Learner Goal:",
       media_url: path,
       )
     sendMessage2(phoneNumber, name)
